4388061f080cc19fb4d62c2add873f44e6809907
enhakkore <ahn.ephraim@gmail.com>
2020-10-27 20:04:46 +0900

    [feat] issueService 추가
    DB에 있는 issue table을 다루는 service를 추가함.
    현재 service에는 모든 issue를 불러오는 readAll method와 특정 issue를 갱신하는 update method가 있음.

---
+++ server/serviecs/issue.service.js
@@ -0,0 +1,24 @@
+const { Issue } = require('../models').models;
+
+module.exports = {
+  readAll: async (req, res, next) => {
+    try {
+      const issues = await Issue.findAll({ where: { deletedAt: null } });
+      res.json(issues);
+    } catch (err) {
+      next(err);
+    }
+  },
+  update: async (req, res, next) => {
+    const wiiBeUpdated = req.body;
+    const issueId = wiiBeUpdated.id;
+    delete wiiBeUpdated.id;
+
+    try {
+      await Issue.update(wiiBeUpdated, { where: { id: issueId } });
+      res.json({ message: '수정 되었습니다.' });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
