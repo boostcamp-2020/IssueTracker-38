ca35fd0b181506c946f70e090898a2cc0934cb5b
Heonjun LEE <dklee730@gmail.com>
2020-10-27 17:47:47 +0900

    [Feat]getLabel 라우팅
    label get api를 위한 라우팅을 함

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // TODO: 라우터 등록
 // router.use('/example', require('./example.route.js'));
+router.use("/label", require("./label"));
 
 module.exports = router;
