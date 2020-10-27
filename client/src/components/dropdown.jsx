fc8f9bdb78db866559dee4108e4ee74ba240300f
eunsol0410 <eunsollee04@gmail.com>
2020-10-28 03:31:15 +0900

    [feat] Dropdown 메뉴 외부 클릭 이벤트 처리
    Dropdown 외부 창을 클릭 시 메뉴가 닫히기 위해서 이벤트 처리하기 위한 useEffect, useRef를 적용하였음
    외부 창 클릭을 확인하기 위해 Wrapper 박스는 css-in-js 활용해 창 전체를 나타내도록 크기를 설정함

--- client/src/components/dropdown.jsx
+++ client/src/components/dropdown.jsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { PropTypes } from 'prop-types';
+import styled from 'styled-components';
 
+const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 function DropdownItem({ id, value }) {
   return (
     <li key={id}>{value}</li>
   );
 }
 export default function Dropdown({ title, items }) {
+  const wrapperRef = useRef(null);
+  const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
   const itemList = items.map((v) => <DropdownItem id={v.id} value={v.value} />);
   const onClick = () => setIsActive(!isActive);
 
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    };
+    if (isActive) {
+      wrapperRef.current.addEventListener('click', pageClickEvent);
+    }
+    return () => {
+      wrapperRef.current.removeEventListener('click', pageClickEvent);
+    };
+  }, [isActive]);
+
   return (
-    <div>
+    <ModalWrapper ref={wrapperRef}>
       <button type="button" onClick={onClick}>
         {title}
         ▼
       </button>
-      <div style={{ display: isActive ? 'block' : 'none' }}>
+      <div ref={dropdownRef} style={{ display: isActive ? 'block' : 'none' }}>
         <p>
           Filter by
           {' '}
@@ -25,7 +46,7 @@ export default function Dropdown({ title, items }) {
         </p>
         <ul>{itemList}</ul>
       </div>
-    </div>
+    </ModalWrapper>
   );
 }
 
