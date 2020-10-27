249a35403441c2a9d4110c27176560c3cbb6fe00
enhakkore <ahn.ephraim@gmail.com>
2020-10-27 20:07:19 +0900

    [feat] router 모듈에 issue router 추가
    전체 router 모듈에 '/issue' 요청을 처리하는 router를 추가함.

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,6 @@ const router = express.Router();
 router.use("/user", require("./user.route.js"));
 router.use("/label", require("./label.route.js"));
 
+router.use('/issue', require('./issue.route'));
+
 module.exports = router;
