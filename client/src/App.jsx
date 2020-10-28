379e214367dcdf9b0fe3606619e2e918f82e6f99
woomin <dal96k@gmail.com>
2020-10-28 16:43:50 +0900

    [Feat] 작업 내용들 병합
    작업물의 결과를 보기 위해 App.jsx에 병합

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Issues from './components/Issues';
+import Dropdown from './components/Dropdown';
+
+import { dummyIssues, dummyDropdownItems } from './dummyData';
+
 export default function App() {
   return (
-    <div>Issue Tracker</div>
+    <>
+      <div>Issue Tracker</div>
+      <Dropdown
+        title="더미"
+        items={dummyDropdownItems}
+      />
+      <Issues
+        issues={dummyIssues}
+      />
+    </>
   );
 }
