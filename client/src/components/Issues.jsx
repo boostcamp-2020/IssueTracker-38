d342df3c0da50bb4447e7879806e50de4ec98a24
Heonjun LEE <dklee730@gmail.com>
2020-10-29 14:43:52 +0900

    [Style]svg 추가

--- client/src/components/Issues.jsx
+++ client/src/components/Issues.jsx
@@ -3,17 +3,35 @@ import { PropTypes } from "prop-types";
 
 import Issue from "./Issue";
 
+const style = {
+  layout: {
+    display: "flex"
+  }
+};
 export default function Issues({ issues, handleCheckboxClick, selections }) {
   return (
     <>
       {issues.map(
         ({ title, milestone, label, issueId, createdAt, asignee }) => (
-          <div key={issueId}>
+          <div css={style.layout} key={issueId}>
             <input
               onClick={() => handleCheckboxClick(issueId)}
               checked={selections.includes(issueId)}
               type="checkbox"
             />
+            <svg
+              class="octicon octicon-issue-opened open"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="16"
+              height="16"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
+              ></path>
+            </svg>
             <Issue
               title={title}
               milestone={milestone}
