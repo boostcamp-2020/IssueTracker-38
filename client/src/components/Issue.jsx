e14cfa95857e60dc984fc07a41301e466d5f98f1
enhakkore <ahn.ephraim@gmail.com>
2020-11-02 18:51:33 +0900

    [feat] 이슈 목록에 상세 표시 추가
    이슈 목록에서 각 이슈에 대한 상세 내용을 추가함.

--- client/src/components/Issue.jsx
+++ client/src/components/Issue.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { PropTypes } from 'prop-types';
+import { UsersContext } from '../stores/UserStore';
+import { MilestoneContext } from '../stores/MilestoneStore';
+import { LabelsContext } from '../stores/LabelStore';
 
 const styles = {
   body: {
@@ -37,25 +40,38 @@ const styles = {
 };
 
 export default function Issue({
+  id,
   title,
-  milestone,
-  label,
-  issueId,
+  userId,
+  milestoneId,
+  labels,
   createdAt,
-  asignee,
 }) {
+  const { users } = useContext(UsersContext);
+  const { milestones } = useContext(MilestoneContext);
+  const { labels: allLabels } = useContext(LabelsContext);
+
+  const userEmail = users.fileter((u) => u.id === userId)[0].email;
+  const milestoneTitle = milestones.filter((m) => m.id === milestoneId)[0].title;
+
   return (
     <div css={styles.body}>
       <div css={styles.layout}>
-        <a css={styles.title} href={`/detail/${issueId}`}>
+        <a css={styles.title} href={`/detail/${id}`}>
           {title}
         </a>
-        <div css={styles.label}>{label}</div>
+        {labels.map((num) => {
+          const target = allLabels.filter((label) => label.id === num)[0];
+          if (target.color) styles.label.backgroundColor = target.color;
+          return (
+            <div css={styles.label}>{target.name}</div>
+          );
+        })}
       </div>
       <div css={styles.details}>
         <div css={styles.detail}>
           #
-          {issueId}
+          {id}
         </div>
         <div css={styles.detail}>
           opened
@@ -65,7 +81,7 @@ export default function Issue({
         <div css={styles.detail}>
           by
           {' '}
-          {asignee}
+          {userEmail}
         </div>
         <svg
           css={styles.milestoneIcon}
@@ -80,17 +96,17 @@ export default function Issue({
             d="M7.75 0a.75.75 0 01.75.75V3h3.634c.414 0 .814.147 1.13.414l2.07 1.75a1.75 1.75 0 010 2.672l-2.07 1.75a1.75 1.75 0 01-1.13.414H8.5v5.25a.75.75 0 11-1.5 0V10H2.75A1.75 1.75 0 011 8.25v-3.5C1 3.784 1.784 3 2.75 3H7V.75A.75.75 0 017.75 0zm0 8.5h4.384a.25.25 0 00.161-.06l2.07-1.75a.25.25 0 000-.38l-2.07-1.75a.25.25 0 00-.161-.06H2.75a.25.25 0 00-.25.25v3.5c0 .138.112.25.25.25h5z"
           />
         </svg>
-        <div css={styles.detail}>{milestone}</div>
+        <div css={styles.detail}>{milestoneTitle}</div>
       </div>
     </div>
   );
 }
 
 Issue.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  milestone: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  issueId: PropTypes.number.isRequired,
+  userId: PropTypes.number.isRequired,
+  milestoneId: PropTypes.number.isRequired,
+  labels: PropTypes.arrayOf(PropTypes.obk).isRequired,
   createdAt: PropTypes.string.isRequired,
-  asignee: PropTypes.string.isRequired,
 };
