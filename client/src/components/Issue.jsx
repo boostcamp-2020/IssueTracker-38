ffa39b42cdcd6ffe2773487b4aef3df1492d5a61
eunsol0410 <eunsollee04@gmail.com>
2020-11-02 23:29:46 +0900

    [Fix] Issue Close/Open 상태 Icon 반영
    isClosed에 따라 아이콘 다르게 보이도록함
    Issue 항목 CSS 일부 다듬기

--- client/src/components/Issue.jsx
+++ client/src/components/Issue.jsx
@@ -7,20 +7,24 @@ const styles = {
   },
   layout: {
     display: 'flex',
-    marginTop: '3px',
+  },
+  statusIcon: {
+    margin: 'auto 5px auto 0',
   },
   title: {
-    fontSize: '20px',
-    fontWeight: 'bold',
+    fontSize: '16px',
+    fontWeight: 'bolder',
     textDecoration: 'none',
     color: 'black',
     marginRight: '10px',
   },
   label: {
-    padding: '5px 10px',
+    padding: '2px 5px',
     backgroundColor: 'violet',
-    borderRadius: '12px',
+    borderRadius: '2px',
     fontSize: '12px',
+    fontWeight: 'bolder',
+    margin: 'auto 0',
   },
   details: {
     display: 'flex',
@@ -43,10 +47,27 @@ export default function Issue({
   issueId,
   createdAt,
   asignee,
+  isClosed,
 }) {
   return (
     <div css={styles.body}>
       <div css={styles.layout}>
+        <svg
+          css={styles.statusIcon}
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          height="16"
+          aria-hidden="true"
+          fill={isClosed ? 'red' : 'green'}
+        >
+          <path
+            fillRule="evenodd"
+            d={isClosed
+              ? 'M7 10h2v2H7v-2zm2-6H7v5h2V4zm1.5 1.5l-1 1L12 9l4-4.5l-1-1L12 7l-1.5-1.5zM8 13.7A5.71 5.71 0 0 1 2.3 8c0-3.14 2.56-5.7 5.7-5.7c1.83 0 3.45.88 4.5 2.2l.92-.92A6.947 6.947 0 0 0 8 1C4.14 1 1 4.14 1 8s3.14 7 7 7s7-3.14 7-7l-1.52 1.52c-.66 2.41-2.86 4.19-5.48 4.19v-.01z'
+              : 'M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z'}
+          />
+        </svg>
         <a css={styles.title} href={`/detail/${issueId}`}>
           {title}
         </a>
@@ -93,4 +114,5 @@ Issue.propTypes = {
   issueId: PropTypes.number.isRequired,
   createdAt: PropTypes.string.isRequired,
   asignee: PropTypes.string.isRequired,
+  isClosed: PropTypes.bool.isRequired,
 };
--- client/src/components/Issues.jsx
+++ client/src/components/Issues.jsx
@@ -14,10 +14,8 @@ const styles = {
       backgroundColor: '#efefef',
     },
   },
-  statusIcon: {
+  checkbox: {
     marginTop: '3px',
-    marginLeft: '10px',
-    fill: 'green',
   },
 };
 export default function Issues({ issues, handleCheckboxClick, selections }) {
@@ -25,27 +23,15 @@ export default function Issues({ issues, handleCheckboxClick, selections }) {
     <>
       {issues.map(
         ({
-          title, milestone, label, issueId, createdAt, asignee,
+          title, milestone, label, issueId, createdAt, asignee, isClosed,
         }) => (
           <div css={styles.layout} key={issueId}>
             <input
+              css={styles.checkbox}
               onChange={() => handleCheckboxClick(issueId)}
               checked={selections.includes(issueId)}
               type="checkbox"
             />
-            <svg
-              css={styles.statusIcon}
-              viewBox="0 0 16 16"
-              version="1.1"
-              width="16"
-              height="16"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
-              />
-            </svg>
             <Issue
               title={title}
               milestone={milestone}
@@ -53,6 +39,7 @@ export default function Issues({ issues, handleCheckboxClick, selections }) {
               issueId={issueId}
               createdAt={createdAt}
               asignee={asignee}
+              isClosed={isClosed}
             />
           </div>
         ),
