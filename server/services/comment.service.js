4de69548c8c8f70823c03df36b4bc971ef9adb37
eunsol0410 <eunsollee04@gmail.com>
2020-11-02 22:18:39 +0900

    [Refactor] Early Return, res.status 설정
    코드 리뷰 반영해서 일부 수정했음

--- server/services/comment.service.js
+++ server/services/comment.service.js
@@ -3,14 +3,15 @@ const { Comment } = require('../models').models;
 module.exports = {
   async create(req, res) {
     await Comment.create(req.body);
-    res.json({ message: '생성 되었습니다.' });
+    res.status(200).json({ message: '생성 되었습니다.' });
   },
   async readByIssue(req, res) {
     const { issueId } = req.query;
-    if (issueId === undefined) res.status(400);
-    else {
-      const comments = await Comment.findAll({ where: { issueId } });
-      res.status(200).json(comments);
+    if (issueId === undefined) {
+      res.status(400).send();
+      return;
     }
+    const comments = await Comment.findAll({ where: { issueId } });
+    res.status(200).json(comments);
   },
 };
