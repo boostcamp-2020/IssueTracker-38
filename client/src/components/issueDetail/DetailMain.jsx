6acb4450a7dc4463fd7f27a865b55879afff6ef5
Heonjun LEE <dklee730@gmail.com>
2020-11-03 22:42:34 +0900

    [feature]DetailTitle로 해당 Issue 전달
    DetailTitle에서 해당 Issue의 정보를 렌더링 해주기 위함

--- client/src/components/issueDetail/DetailMain.jsx
+++ client/src/components/issueDetail/DetailMain.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 import { IssuesContext } from '../../stores/IssueStore';
@@ -7,10 +7,17 @@ import DetailTitle from './DetailTitle';
 export default function DetailMain() {
     const { issues } = useContext(IssuesContext);
     const { issueId } = useParams();
+    const [issue, setIssue] = useState();
+
+    useEffect(() => {
+        const mathched = issues.find((v) => v.id === +issueId);
+        setIssue(mathched);
+    }, [issues]);
+
     return (
         <>
             <DetailTitle
-                issue={issues.find((issue) => issue.id === +issueId)}
+                issue={issue}
             />
         </>
     );
