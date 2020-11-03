100ab4e8feb08d60f6c8d94783085aaf7b4653b1
woomin <dal96k@gmail.com>
2020-11-03 18:32:32 +0900

    [Feat] Assignee와 Label 지정에 대해 서버와 연결 완료
    issueAPI에 있던 update 함수를 사용하여 Assignee와 Label에 한해 지정 혹은
    이미 지정되어있다면 지정해제가 가능하도록 구현함.

--- client/src/components/issueDetail/SideBarItemDropdown.jsx
+++ client/src/components/issueDetail/SideBarItemDropdown.jsx
@@ -3,6 +3,8 @@ import { PropTypes } from 'prop-types';
 
 import { useParams } from 'react-router-dom';
 
+import { issueAPI } from '../../apis/api';
+
 const styles = {
   layout: {
     position: 'absolute',
@@ -21,11 +23,20 @@ export default function SideBarItemDropdown({ items, assigned, title }) {
 
   const isAlreadyAssigned = (id) => assigned.find((base) => base.id === id);
 
-  const handleAssigning = (id) => () => {
-    /* TODO: API 로직 구현
-     * isAlreadyAssigned(id) 함수를 사용하여 새롭게 추가하는 건지, 제거하는 건지 구분
-     * issueId에 해당하는 항목(title : Assignee, Label, Milestone)을 골라서 id에 맞게 어사인해줌
-     */
+  const handleAssigning = (id) => async () => {
+    const type = isAlreadyAssigned(id) ? 'delete' : 'add';
+
+    if (title === 'Assignees') {
+      issueAPI.update({ id: issueId, assignee: { type, id } });
+    }
+
+    if (title === 'Labels') {
+      issueAPI.update({ id: issueId, label: { type, id } });
+    }
+
+    if (title === 'Milestone') {
+      // TODO: 구현
+    }
   };
 
   return (
