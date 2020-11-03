1f7bacde32d270394cfec10138f1261fc5abdb03
enhakkore <ahn.ephraim@gmail.com>
2020-11-04 01:10:13 +0900

    [refactor] 경과시간 계산할 때 유틸 사용
    경과시간을 계산할때 utils를 사용함.

--- client/src/components/issueList/Issue.jsx
+++ client/src/components/issueList/Issue.jsx
@@ -3,6 +3,7 @@ import { PropTypes } from 'prop-types';
 import { UsersContext } from '../../stores/UserStore';
 import { MilestoneContext } from '../../stores/MilestoneStore';
 import { LabelsContext } from '../../stores/LabelStore';
+import { calElapsedTime } from '../../utils/utils';
 
 const styles = {
   body: {
@@ -41,21 +42,6 @@ const styles = {
   milestoneIcon: {
     fill: 'grey',
   },
-};
-
-const calElapsedTime = (createdAt) => {
-  const createdTime = new Date(createdAt);
-
-  const diff = Date.now() - createdTime.getTime();
-  const seconds = diff / 1000;
-  const minutes = seconds / 60;
-  const hours = minutes / 60;
-  const days = hours / 24;
-
-  if (days >= 1) return `${Math.floor(days)} days`;
-  if (hours >= 1) return `${Math.floor(hours)} hours`;
-  if (minutes >= 1) return `${Math.floor(minutes)} minutes`;
-  return `${Math.floor(seconds)} seconds`;
 };
 
 export default function Issue({
