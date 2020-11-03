f4fdf13130eb3f6e623229d566a023c0eed2e3ec
eunsol0410 <eunsollee04@gmail.com>
2020-11-03 23:03:30 +0900

    [Feat] 댓글 생성 관련 CSS 적용
    profile 이미지는 임시로 div를 사용해서 구현함
    input에 comment 입력시 css 변화되는 부분이 아직 빠져있음

--- client/src/components/issueDetail/NewComment.jsx
+++ client/src/components/issueDetail/NewComment.jsx
@@ -5,16 +5,70 @@ import { IssuesContext } from '../../stores/IssueStore';
 
 const styles = {
   body: {
+    marginTop: '10px',
+    display: 'flex',
   },
   profile: {
+    width: '40px',
+    height: '40px',
+    backgroundColor: 'skyblue',
+    borderRadius: '3px',
   },
   layout: {
+    marginLeft: '20px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '800px',
+    border: '1px solid #e1e4e8',
+    borderRadius: '6px 6px 0 0',
+  },
+  inputTitle: {
+    borderBottom: '1px solid #e1e4e8',
+    backgroundColor: '#f6f8fa',
+    width: '100%',
+  },
+  titleContent: {
+    backgroundColor: 'white',
+    width: '60px',
+    margin: '10px 10px -1px 10px',
+    padding: '5px 0',
+    textAlign: 'center',
+    border: '1px solid #e1e4e8',
+    borderRadius: '6px 6px 0 0',
+    borderBottom: '0',
   },
   inputWrapper: {
+    width: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  input: {
+    border: '1px solid #e1e4e8',
+    borderRadius: '6px',
+    boxShadow: '0px 0px 5px #e1e4e8',
+    width: '100%',
+    height: '200px',
+    margin: '20px 10px 10px 10px',
+    '&:focus': {
+      backgroundColor: '#f6f8fa',
+      outline: 'none',
+    },
   },
   statusButton: {
+    margin: '0 10px 10px auto',
+    padding: '5px 16px',
+    borderRadius: '6px',
+    border: '1px solid #e1e4e8',
+    fontWeight: '600',
   },
   commentButton: {
+    backgroundColor: '#94d3a2', // #2ea44f
+    color: 'white',
+    padding: '5px 16px',
+    margin: '0 10px 10px 10px',
+    borderRadius: '6px',
+    border: '1px solid #e1e4e8',
+    fontWeight: '600',
   },
 };
 
@@ -46,10 +100,15 @@ export default function NewComment({ user, issue }) {
 
   return (
     <div css={styles.body}>
-      <div css={styles.profile} />
+      <div css={styles.profile}>
+        &nbsp;
+      </div>
       <div css={styles.layout}>
+        <div css={styles.inputTitle}>
+          <p css={styles.titleContent}>Write</p>
+        </div>
         <div css={styles.inputWrapper}>
-          <input type="text" ref={inputRef} />
+          <input type="text" css={styles.input} ref={inputRef} placeholder="Leave a Comment" />
           <button css={styles.statusButton} type="submit" onClick={changeIssueStatus}>
             {issue?.isClosed ? 'Reopen issue' : 'Close issue'}
           </button>
