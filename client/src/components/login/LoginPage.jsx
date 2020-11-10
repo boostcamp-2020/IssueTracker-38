2187484359f921d6804911b1f3407912c765b008
Heonjun LEE <dklee730@gmail.com>
2020-11-10 18:34:23 +0900

    [Feat]로그인 여부 확인 후 리다이렉팅 구현

--- client/src/components/login/LoginPage.jsx
+++ client/src/components/login/LoginPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import LoginMain from './container/LoginMain';
 
+import { Redirect } from 'react-router-dom';
+
 export default function LoginPage() {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    return <Redirect to='/' />
+  }
+
   return (
     <LoginMain />
   );
