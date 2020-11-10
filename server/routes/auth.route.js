4354ea1d2bc726a8d2942360882a58fee0d7c988
eunsol0410 <eunsollee04@gmail.com>
2020-11-10 22:53:14 +0900

    [chore] server lint 반영함

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authService = require('../services/auth.service');
 const errorCatcher = require('../middlewares/errorCatcher');
-const authCheck = require('../middlewares/authCheck')
+const authCheck = require('../middlewares/authCheck');
 
 router.post('/', errorCatcher(authService.getAcessToken));
 router.post('/fresh', authCheck, errorCatcher(authService.getFreshAcessToken));
