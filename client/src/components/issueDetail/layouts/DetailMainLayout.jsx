f16df0b7224b4df0e5e7901ab2937fa96c7d1041
eunsol0410 <eunsollee04@gmail.com>
2020-11-10 23:28:40 +0900

    [Refactor] DetailMain Layout 분리

---
+++ client/src/components/issueDetail/layouts/DetailMainLayout.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const WrapperLayout = styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 1200px;
+box-sizing: border-box;
+margin: 0 auto;
+`;
+
+export const ContentLayout = styled.div`
+width: 1200px;
+display: flex;
+`;
