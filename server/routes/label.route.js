49c50912cde95470b1556cff15dd2fe394e2f18f
eunsol0410 <eunsollee04@gmail.com>
2020-11-11 15:24:11 +0900

    [Feat] 라벨 생성 API 구현

--- server/routes/label.route.js
+++ server/routes/label.route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const labelService = require('../services/label.service');
 const errorCatcher = require('../middlewares/errorCatcher');
 
+router.post('/', errorCatcher(labelService.create));
 router.get('/', errorCatcher(labelService.readAll));
 
 module.exports = router;
--- server/services/label.service.js
+++ server/services/label.service.js
@@ -1,6 +1,12 @@
 const { Label } = require('../models').models;
 
 module.exports = {
+  async create(req, res) {
+    const newLabel = req.body;
+    const createdLabel = await Label.create(newLabel);
+
+    res.status(200).json(createdLabel);
+  },
   async readAll(req, res) {
     const labels = await Label.findAll();
 
