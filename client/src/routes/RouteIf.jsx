8c48aef862d781d803feda6c821f71e262fe241b
woomin <dal96k@gmail.com>
2020-11-11 14:39:47 +0900

    [Feat] 라벨 페이지 임시 생성 및 라우팅 적용
    RouteIf에서 Redirect 로직이 있으면 새로고침 시 무조건 "/" 경로로 가는
    문제가 있어서 잠시 제거함

--- client/src/routes/RouteIf.jsx
+++ client/src/routes/RouteIf.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import StoreWrapper from '../stores/StoreWrapper';
 import Header from '../components/Header';
 import DetailMain from '../components/issueDetail/IssueDetailPage';
 import IssueMain from '../components/issueList/IssueMain';
 import NewIssueMain from '../components/newIssue/NewIssueMain';
+import LabelPage from '../components/labelList/LabelPage';
 
 export default function RouteIf({ path, exact = false }) {
   return (
-    <Route exact={exact} path={path} render={() => {
-      if (localStorage.getItem('accessToken')) {
-        return (
-          <Router>
-            <Switch>
-              <StoreWrapper>
-                <Header />
-                <Route exact path="/" component={IssueMain} />
-                <Route path="/new-issue" component={NewIssueMain} />
-                <Route path="/detail/:issueId" component={DetailMain} />
-                <Redirect path="*" to="/" />
-              </StoreWrapper>
-            </Switch>
-          </Router>
-        );
-      }
+    <Route
+      exact={exact}
+      path={path}
+      render={() => {
+        if (localStorage.getItem('accessToken')) {
+          return (
+            <Router>
+              <Switch>
+                <StoreWrapper>
+                  <Header />
+                  <Route exact path="/" component={IssueMain} />
+                  <Route path="/new-issue" component={NewIssueMain} />
+                  <Route path="/detail/:issueId" component={DetailMain} />
+                  <Route path="/label" component={LabelPage} />
+                  {/* <Redirect path="*" to="/" /> */}
+                </StoreWrapper>
+              </Switch>
+            </Router>
+          );
+        }
 
-      return <Redirect to="/login" />;
-    }}
+        return <Redirect to="/login" />;
+      }}
     />
   );
 }
@@ -36,4 +43,4 @@ RouteIf.propTypes = {
   path: PropTypes.string.isRequired,
   exact: PropTypes.bool.isRequired,
   component: PropTypes.shape.isRequired,
-}
+};
