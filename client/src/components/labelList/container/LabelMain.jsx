73d69163a7d6564a4264cd6390cd6ce98785a8f0
Heonjun LEE <dklee730@gmail.com>
2020-11-11 18:12:10 +0900

    [Feat]라벨 인풋 형식에 color와 preview추가

--- client/src/components/labelList/container/LabelMain.jsx
+++ client/src/components/labelList/container/LabelMain.jsx
@@ -1,24 +1,60 @@
-import React,{ useState } from 'react';
+import React,{ useState,useContext } from 'react';
 import LabelInputForm from '../presentational/LabelInputForm';
 import DefaultButton from '../../issueDetail/presentational/DefaultButton';
 import LabelOrMilestoneButton from '../presentational/LableOrMilestoneButton';
+import { useInput } from '../../../hooks/hooks';
+import { getRandomColorCode } from '../../../utils/utils';
+import { labelAPI } from '../../../apis/api';
+import { LabelStore,LabelsContext } from '../../../stores/LabelStore'
+
+const styles = {
+  newLabelButton:{
+    backgroundColor:'green'
+  }
+}
 
 export default function LableMain() {
-  const styles = {
-    newLabelButton:{
-      backgroundColor:'green'
-    }
-}
-const [newLabel,setNewLabel]= useState(0);
-const showCreateLabelInput = () =>{
-  newLabel?setNewLabel(0):setNewLabel(1)
-  return;
-}
+  const [newLabel,setNewLabel]= useState(0);
+  const [labelName,setLabelName] = useInput('');
+  const [labelColor,setLabelColor] = useState(getRandomColorCode());
+  const [labelDescription, setLabelDescription] = useInput('');
+  const {dispatch} = useContext(LabelsContext);
+
+  const labelData = {
+    name:labelName,
+    color:labelColor,
+    description:labelDescription
+  }
+  const showCreateLabelInput = () =>{
+    newLabel?setNewLabel(0):setNewLabel(1)
+    return;
+  }
+  const makeRandomColor = () =>{
+    setLabelColor(getRandomColorCode());
+  }
+  const onChangeColor = (e) =>{
+    setLabelColor(e.target.value)
+  }
+  const createLabel = async ()=>{
+    const result = await labelAPI.create(labelData);
+    dispatch({type:'ADD',payload:result});
+  }
+
   return (<>
-      <LabelOrMilestoneButton></LabelOrMilestoneButton>
+      <LabelOrMilestoneButton />
       <DefaultButton text='New Label' onClick={showCreateLabelInput} extraStyle={styles.newLabelButton}></DefaultButton>
       {newLabel
-        ?<LabelInputForm saveText='Create Label' onClick={showCreateLabelInput}></LabelInputForm>
+        ?<LabelInputForm 
+          labelName={labelName} 
+          description={labelDescription} 
+          color={labelColor} 
+          saveText='Create Label' 
+          onClick={makeRandomColor}
+          onChangeName={setLabelName}
+          onChangeDescription={setLabelDescription}
+          onChangeColor={onChangeColor}
+          onSave={createLabel}
+        />
         :<div></div>}
     </>
   );
--- client/src/components/labelList/presentational/LabelInputForm.jsx
+++ client/src/components/labelList/presentational/LabelInputForm.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import DefaultButton from '../../issueDetail/presentational/DefaultButton';
+import LabelPreview from '../presentational/LabelPreview';
+import ColorRefreshButton from '../presentational/ColorRefreshButton';
+import ColorChangeInput from '../presentational/ColorChangeInput';
 
 const styles = {
   inputForm:{
       display:'flex',
-      flexDirection:'row',
+      flexDirection:'column',
       justifyContent:'space-between',
       width:'100%',
       height:'150px',
@@ -38,22 +41,42 @@ const styles = {
   }
   }
 
-export default function LabelInputForm({ labelName, description,color,saveText,onClick}) {
+export default function LabelInputForm({
+  labelName,
+  description,
+  color,
+  saveText,
+  onClick,
+  onChangeName,
+  onChangeDescription,
+  onChangeColor,
+  onSave,
+}) {
     return(
       <div css={styles.inputForm}>
+        <div>
+          {!labelName
+            ?<LabelPreview name={'Label preview'} color={color} />
+            :<LabelPreview name={labelName} color={color} />}
+        </div>
         <div css={styles.inputs}>
           <div css={styles.labelNameInput}>
             LabeName
-            <textarea value={labelName}></textarea>
+            <textarea value={labelName} onChange={onChangeName}></textarea>
           </div>
           <div css={styles.labelDescriptionInput}>
             Description
-            <textarea value={description}></textarea>
+            <textarea value={description} onChange={onChangeDescription}></textarea>
+          </div>
+          <div>
+            Color
+            <ColorRefreshButton backgroundColor={color} onClick={onClick}/>
+            <ColorChangeInput value={color} onChange={onChangeColor}/>
           </div>
         </div>
         <div css={styles.buttons}>
           <DefaultButton text='cancel' extraStyle={styles.cancelButton}></DefaultButton>
-          <DefaultButton text={saveText} extraStyle={styles.createLabelButton} onClick={onClick}></DefaultButton>
+          <DefaultButton text={saveText} extraStyle={styles.createLabelButton} onClick={onSave}></DefaultButton>
         </div>
       </div>
     )
