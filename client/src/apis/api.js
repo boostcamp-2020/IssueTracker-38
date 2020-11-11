3d95a4eb141674400239db991c3b05e5909fd97f
eunsol0410 <eunsollee04@gmail.com>
2020-11-11 15:50:59 +0900

    [Feat] 클라이언트 라벨 새 API 연결 구현

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -103,6 +103,18 @@ export const milestoneAPI = {
 };
 
 export const labelAPI = {
+  async create() {
+    const url = `${baseURL}/label`;
+    const request = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const result = await customFetch(url, request);
+    return result;
+  },
   async readAll() {
     const url = `${baseURL}/label`;
     const request = {
@@ -111,6 +123,27 @@ export const labelAPI = {
     };
     const labels = await customFetch(url, request);
     return labels;
+  },
+  async update() {
+    const url = `${baseURL}/label`;
+    const request = {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const result = await customFetch(url, request);
+    return result;
+  },
+  async remove() {
+    const url = `${baseURL}/label`;
+    const request = {
+      method: 'DELETE',
+      headers: { Authorization: `Bearer ${localStorage.getItem('accessToken')}` },
+    };
+    const result = await customFetch(url, request);
+    return result;
   },
 };
 
